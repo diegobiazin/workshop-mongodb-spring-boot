f16db4a001327155553130cbee1f224ac90994ce
Diego Biazin <diego_biazin@hotmail.com>
2018-07-17 23:00:10 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/diegobiazin/workshopmongodb/WorkshopmongodbApplication.java
@@ -0,0 +1,12 @@
+package com.diegobiazin.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongodbApplication.class, args);
+	}
+}
