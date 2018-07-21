c80406f4bc9e17d1d3f58bcbfab6bc29e801f75d
Diego Biazin <diego_biazin@hotmail.com>
2018-07-20 23:25:29 -0300

    Consulta com vários critérios @Query

--- src/main/java/com/diegobiazin/workshopmongodb/services/PostService.java
+++ src/main/java/com/diegobiazin/workshopmongodb/services/PostService.java
@@ -10,6 +10,7 @@ import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +25,12 @@ public class PostService {
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public List<Post> findByTitle(String text){
+    public List<Post> findByTitle(String text) {
         return repo.findByTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
